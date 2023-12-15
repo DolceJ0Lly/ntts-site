cb67ccdef5b4fb752d45a160043b4e533a610e96
FiokoLy <78534917+DolceJ0Lly@users.noreply.github.com>
2023-12-15 12:10:34 +0100

    added Italian Translation

--- translations/text.js
+++ translations/text.js
@@ -12,6 +12,7 @@ export const languageMap = {
 export const titleMap = {
   "en-US": "No Text To Speech",
   "eg-AR": "No Text To Speech",
+  "it-IT": "No Text To Speech",
 };
 
 /** @type {Readonly<Record<Locale, {lightweight:string;realtime?:string;suspense?:string;pagination?:string;backendAgnostic?:string;renderingStrategies?:string;typescript?:string;remoteLocal?:string;}>>} */
@@ -27,7 +28,12 @@ export const featuresMap = {
     rules: "القوانين",
     discord: "ديسكورد",
     yt: "يوتيوب",
-  }
+  },
+  "it-IT": {
+    mail: "Email",
+    faq: "Domande Frequenti",
+    discord: "Discord",
+    yt: "YouTube",
 };
 
 /** @type {Readonly<Record<Locale, string>>} */
@@ -36,6 +42,8 @@ export const headDescriptionMap = {
     "No Text To Speech website.",
   "eg-AR":
     "No Text To Speech website.",
+  "it-IT":
+    "No Text To Speech sito web.",
 };
 
 /** @type {Readonly<Record<Locale, string>>} */
@@ -47,6 +55,7 @@ export const feedbackLinkMap = {
   ko: "질문이 있으신가요? 피드백을 남겨주세요 →",
   ru: "Вопросы? Оставьте нам отзыв →",
   "eg-AR": "Question? Give us feedback →",
+  "it-IT": "Domande? Dacci un feedback →",
 };
 
 /** @type {Readonly<Record<Locale, string>>} */
@@ -59,6 +68,7 @@ export const editTextMap = {
   ko: "Github에서 이 페이지 편집하기 →",
   ru: "Редактировать эту страницу на GitHub →",
   "eg-AR": "Edit this page on GitHub →",
+  "it-IT": "Modifica questa pagina su GitHub →",
 };
 
 /** @type {Readonly<Record<Locale, { utmSource: string; text: string; suffix?: string | undefined }>>} */
@@ -73,6 +83,7 @@ export const tableOfContentsTitleMap = {
   "pt-BR": "Nessa página",
   ru: "На этой странице",
   "eg-AR": "On This Page",
+  "it-IT": "Su Questa Pagina",
 };
 
 /** @type {Readonly<Record<Locale, string>>} */
@@ -83,10 +94,12 @@ export const searchPlaceholderMap = {
   "pt-BR": "Buscar documentação...",
   ko: "문서 검색...",
   ru: "Искать в документации...",
+  "it-IT": "Cerca il sito...",
 };
 
 /** @type {Readonly<Record<Locale, string>>} */
 export const gitTimestampMap = {
   "en-US": "Last updated on",
   ru: "Последнее обновление",
+  "it-IT": "Ultima volta aggiornato il",
 };
